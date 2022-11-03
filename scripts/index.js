4f1b296f8d7c91c11056b055183f608da7f69642
TouristOnAMission <maikl.mikhael@yandex.ru>
2022-11-04 00:53:43 +0300

    feat: Сделать исправления

--- scripts/index.js
+++ scripts/index.js
@@ -1,40 +1,11 @@
-const popupElem = document.querySelector('.popup'),
-      editElem = document.querySelector('.profile__editor'),
-      profileName = document.querySelector('.profile__name'),
-      profileDescription = document.querySelector('.profile__description'),
-      formElement = document.querySelector('.popup__form'),
-      nameInput = formElement.querySelector('.popup__input-username'),
-      descriptionInput = formElement.querySelector('.popup__input-description'),
-      cardElements = document.querySelector('.cards');
-
-editElem.addEventListener('click', () => {
-  onOpen(popupElem);
-});
-
-popupElem.addEventListener('click', (event) => {
-  const isOverlay = event.target.classList.contains('popup_opened'),
-        isClose = event.target.classList.contains('popup__close'),
-        isButton = event.target.classList.contains('popup__button-save');
-
-  if(isOverlay || isClose || isButton) {
-    onClose(popupElem);
-  }
-});
-
-function formSubmitHandler (evt) {
-    evt.preventDefault();
-
-    profileName.textContent = nameInput.value;
-    profileDescription.textContent = descriptionInput.value;
-}
-
-formElement.addEventListener('submit', formSubmitHandler);
-
-cardElements.addEventListener('click', (event) => {
-  if (event.target.classList.contains('cards__like')) {
-    event.target.classList.toggle('cards__like-active');
-  }
-});
+const popupElem = document.querySelector('.popup');
+const editElem = document.querySelector('.profile__editor');
+const profileName = document.querySelector('.profile__name');
+const profileDescription = document.querySelector('.profile__description');
+const formElement = document.querySelector('.popup__form');
+const nameInput = formElement.querySelector('.popup__input-username');
+const descriptionInput = formElement.querySelector('.popup__input-description');
+const popupClose = popupElem.querySelector('.popup__close');
 
 function onOpen(popup) {
   popup.classList.add('popup_opened');
@@ -43,3 +14,19 @@ function onOpen(popup) {
 function onClose(popup) {
   popup.classList.remove('popup_opened');
 }
+
+function formSubmitHandler (evt) {
+  evt.preventDefault();
+
+  profileName.textContent = nameInput.value;
+  profileDescription.textContent = descriptionInput.value;
+  onClose(popupElem);
+}
+
+editElem.addEventListener('click', () => {
+  onOpen(popupElem);
+});
+
+formElement.addEventListener('submit', formSubmitHandler);
+
+popupClose.addEventListener('click', formSubmitHandler);
